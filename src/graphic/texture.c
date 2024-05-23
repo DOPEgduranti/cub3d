7e4d78cdc090911b5f84c0d64361a9800d3b3d5b
DUPEgduranti <gduranti@student.42firenze.it>
2024-05-23 11:24:13 +0200

    pusha intanto

--- src/graphic/texture.c
+++ src/graphic/texture.c
@@ -6,7 +6,7 @@
 /*   By: gduranti <gduranti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 11:30:33 by gduranti          #+#    #+#             */
-/*   Updated: 2024/05/22 16:31:23 by gduranti         ###   ########.fr       */
+/*   Updated: 2024/05/23 11:23:33 by gduranti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,7 +65,7 @@ void	update_pixels(t_data *data, t_textures *tex, t_ray *ray, int x)
 	{
 		tex->y = (int)tex->pos & (tex->size - 1);
 		tex->pos += tex->step;
-		color = data->pixels[tex->index][tex->size * tex->y + tex->x];
+		color = data->textures.txtrs[tex->index][tex->size * tex->y + tex->x];
 		if (tex->index == NORTH || tex->index == EAST)
 			color = (color >> 1) & 8355711;
 		if (color > 0)
